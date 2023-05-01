e36c99dfd9111b1e52609223785852767e492e04
riogor <astronaft100@yandex.ru>
2023-05-01 22:08:16 +0300

    Add chosen puzzle loading

--- main.sh
+++ main.sh
@@ -23,12 +23,17 @@ nonograms_path_list=( $( find ./ext -name "*.non") )
 
 nonograms_list=( )
 
-function load_puzzle {
+function check_puzzle {
 	_input=$1
 	_title=""
 	_w=0
 	_h=0
 	_colored=false
+
+	_max_rowlen=0
+	_max_collen=0
+	_rows_cnt=$INF
+	_col_cnt=$INF
 	
 	while IFS= read -r line
 	do
@@ -50,16 +55,7 @@ function load_puzzle {
 		if [[ "$line" =~ ^color ]]; then
 			_colored=true
 		fi
-		
-	done < "${_input}"
-	
-	_max_rowlen=0
-	_max_collen=0
-	_rows_cnt=$INF
-	_col_cnt=$INF
-	
-	while IFS= read -r line
-	do
+
 		if [[ $_rows_cnt -lt $_h ]]; then
 			if [[ "$line" =~ [0-9]+[a-z] ]]; then
 				_colored=true
@@ -67,7 +63,6 @@ function load_puzzle {
 				_currow=$( echo "$line" | tr -cd ',' | wc -m )
 				_currow=$(( $_currow + 1 ))
 				_max_rowlen=$(( $_max_rowlen > $_currow ? $_max_rowlen : $_currow ))
-				a=3
 			fi
 			
 			_rows_cnt=$(( $_rows_cnt + 1 ))
@@ -92,12 +87,21 @@ function load_puzzle {
 		if [[ "$line" =~ ^columns ]]; then
 			_col_cnt=0
 		fi
+		
 	done < "${_input}"
 	
 	# echo $item $_colored $_max_collen $_max_rowlen
 	
 	# see layout for explanations
 	if [[ ( "$_colored" = false ) && ( $term_w -gt $(( $_w + 1 + $_max_rowlen )) ) && ( $term_h -gt $(( $_h + 1 + 1 + $_max_collen )) ) ]]; then
+		return 0
+	else
+		return 1
+	fi
+};
+
+function load_puzzle {
+	if check_puzzle $1; then
 		nonograms_list+=( "${_title} (${_w}x${_h})" )
 	fi
 };
@@ -107,7 +111,9 @@ for item in ${nonograms_path_list[@]}; do
 	load_puzzle ${item} 
 done
 
-echo Welcome to NonoCon - nonogram game in console
+clear
+
+echo Welcome to NonoCon - nonogram game in console!
 echo "Choose a puzzle from the list below (some puzzles may be excluded because they won't fit into your terminal):"
 
 select chosen in "${nonograms_list[@]}"
@@ -119,5 +125,91 @@ do
 	fi
 done
 
+puzzle_name=$chosen
 puzzle_number=$(($REPLY - 1))
-echo $(($puzzle_number + 1)) : $chosen ${nonograms_path_list[$puzzle_number]}
+puzzle_path=${nonograms_path_list[$puzzle_number]}
+echo $(($puzzle_number + 1)) : $puzzle_name $puzzle_path
+
+clear
+echo Loading $chosen...
+
+check_puzzle ${puzzle_path}
+
+w=$_w
+h=$_h
+max_rowlen=$max_rowlen
+max_collen=$_max_collen
+
+_rows_cnt=$INF
+_col_cnt=$INF
+rows=( )
+cols=( )
+
+field=( )
+goal=( )
+
+while IFS= read -r line
+do		
+	if [[ "$line" =~ ^goal ]]; then
+		[[ "$line" =~ \"(.+)\" ]]
+		
+		goal_str=${BASH_REMATCH[1]}
+		for (( i = 0; i < ${#goal_str}; ++i )); do
+			goal+=( ${goal_str:i:1} )
+		done
+	fi
+
+	if [[ $_rows_cnt -lt $_h ]]; then
+		_tmparr=($( echo "$line" | tr ',' '\n'))
+		
+		for i in ${!_tmparr[@]}; do
+			rows+=( "${_tmparr[$i]}" )
+		done
+
+		for (( i = 0; i < $(( max_rowlen - ${#_tmparr[@]} )); ++i )); do
+			rows+=( "0" )
+		done
+
+		_rows_cnt=$(( $_rows_cnt + 1 ))
+	fi
+
+	if [[ $_col_cnt -lt $_w ]]; then
+		_tmparr=($( echo "$line" | tr ',' '\n'))
+		
+		for i in ${!_tmparr[@]}; do
+			cols+=( "${_tmparr[$i]}" )
+		done
+
+		for (( i = 0; i < $(( max_collen - ${#_tmparr[@]} )); ++i )); do
+			cols+=( "0" )
+		done
+		
+		_col_cnt=$(( $_col_cnt + 1 ))
+	fi
+
+	if [[ "$line" =~ ^width ]]; then
+		[[ "$line" =~ [0-9]+ ]]
+		_w=${BASH_REMATCH[0]}
+	fi
+	
+	if [[ "$line" =~ ^height ]]; then
+		[[ "$line" =~ [0-9]+ ]]
+		_h=${BASH_REMATCH[0]}
+	fi
+
+	if [[ "$line" =~ ^rows ]]; then
+		_rows_cnt=0
+	fi
+
+	if [[ "$line" =~ ^columns ]]; then
+		_col_cnt=0
+	fi
+
+done < "${puzzle_path}"
+
+field=(${goal[@]})
+for i in ${!field[@]}; do
+	field[$i]=0
+done
+
+clear
